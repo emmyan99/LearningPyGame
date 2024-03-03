eefa7f67564a601ea5d6409af6d17117cfd173db
emmyan99 <emmy.andreasson@gmail.com>
2024-03-03 20:19:33 +0100

    foodCollision

--- snake.py
+++ snake.py
@@ -4,6 +4,7 @@ import random
 def main():
     # pygame setup
     pygame.init()
+
     screen = pygame.display.set_mode((720, 720))
     clock = pygame.time.Clock()
     running = True
@@ -11,8 +12,7 @@ def main():
     score=0
     screen.fill("black")
     snake = pygame.draw.rect(screen, "green", (360, 360, 20, 20), 0)
-    foodX = random.randint(0,720)
-    foodY = random.randint(0,720)
+    food = pygame.draw.circle(screen, "orange", (random.randint(0,720), random.randint(0,720)), 4)
 
     while running:
         for event in pygame.event.get():
@@ -22,24 +22,21 @@ def main():
         # wipe last frame
         screen.fill("black")
 
-        food = pygame.draw.circle(screen, "orange", (foodX, foodY), 4)
+        food = pygame.draw.circle(screen, "orange", (food.centerx, food.centery), 4)
         snake = pygame.draw.rect(screen, "green", (snake.left, snake.top, snake.width, snake.height), 0)
         
         keys = pygame.key.get_pressed()
-        #if (sum(keys) > 1):
-        #    print(sum(keys))
-        #    snake.move_ip(0, 0)
-        if keys[pygame.K_w]:
-            snake.move_ip(0, -dt*300)
-        elif keys[pygame.K_s]:
-            snake.move_ip(0, dt*300)
-        elif keys[pygame.K_a]:
-            snake.move_ip(-dt*300, 0)
+        if keys[pygame.K_a]:
+            snake.move_ip(-dt * 300, 0)
         elif keys[pygame.K_d]:
-            snake.move_ip(dt*300, 0)
+            snake.move_ip(dt * 300, 0)
+        elif keys[pygame.K_w]:
+            snake.move_ip(0, -dt * 300)
+        elif keys[pygame.K_s]:
+            snake.move_ip(0, dt * 300)
 
         edgeCollision(snake.left, snake.top, snake.width)
-        #foodCollision(screen)
+        foodCollision(snake, food, score)
 
         # flip() the display to put your work on screen
         pygame.display.flip()
@@ -55,6 +52,16 @@ def edgeCollision(x, y, width):
     if (x > 720-width) or (x < 0) or (y > 720-width) or (y < 0):
         print("Collision with edge. Fail.")
         main()
+
+#TODO fix food spawn
+def foodCollision(snake, food, score):
+    if snake.colliderect(food):
+        food.move_ip(random.randint(0,100), random.randint(0,100))
+        score += 100
+
+
+
+
 
 
 main()

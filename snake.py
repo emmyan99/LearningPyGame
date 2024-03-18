7507d567089064252e2b49c74702c3790dba7656
emmyan99 <emmy.andreasson@gmail.com>
2024-03-18 18:02:33 +0100

    snake can grow to 2 rectanlges :<)

--- snake.py
+++ snake.py
@@ -1,9 +1,9 @@
 import pygame
 import random
 
-#TODO: NAME COMMON VARIABLES
-#TODO: INCREMENT SIZE OF SNAKE, REMOVE UNION AND USE SEVERAL RECTANGLES
-#TODO: FIX MOVEMENT OF LARGER SNAKE
+#TODO: ADD TAIL TO TAIL, recursive? 
+#TODO: MOVEMENT OF LARGER SNAKE, tail must stay behind head
+#TODO: INDEPENDENT MOVEMENT OF BODY
 #TODO: COLLISION WITH SELF 
 #TODO: FIX SNAKE AUTOMATIC MOVEMENT
 #TODO: LEADERBOARD
@@ -11,21 +11,25 @@ import random
 def main():
     # pygame setup
     pygame.init()
-
-    screen = pygame.display.set_mode((720, 720))
+    SCREEN_WIDTH = 720
+    SCREEN_HEIGHT = 720
+    GRID_SIZE = 30
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
     score = 0
     screen.fill("black")
-    snake = pygame.draw.rect(screen, "green", (360, 360, 30, 30), 0)
-    foodX = 15+(30*(random.randint(0,23)))
-    foodY = 15+(30*(random.randint(0,23)))
+    snake = pygame.draw.rect(screen, "green", (SCREEN_WIDTH/2, SCREEN_WIDTH/2, GRID_SIZE, GRID_SIZE), 0)
+    foodX = GRID_SIZE//2 + (GRID_SIZE * (random.randint(0, SCREEN_WIDTH//GRID_SIZE-1)))
+    foodY = GRID_SIZE//2 + (GRID_SIZE * (random.randint(0, SCREEN_HEIGHT//GRID_SIZE-1)))
     food = pygame.draw.circle(screen, "orange", (foodX, foodY), 6)
-    moving = False
     lastDir = (0,0)
     iterationCounter = 0
-    EASY = 60
-    MEDIUM = 40
-    HARD = 20
+    snakeSize = 1
+    tailPosition = [] # list of tail rect positions
+    tails = [] #list of tail rects
+    EASY = 200
+    MEDIUM = 80
+    HARD = 40
     difficulty = EASY
 
     while running:
@@ -33,6 +37,7 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         
+        keys = pygame.key.get_pressed()
         iterationCounter +=1
 
         # wipe last frame
@@ -40,21 +45,28 @@ def main():
 
         food = pygame.draw.circle(screen, "orange", (food.centerx, food.centery), 14)
         snake = pygame.draw.rect(screen, "green", (snake.left, snake.top, snake.width, snake.height), 0)
-        
-        keys = pygame.key.get_pressed()
-        moving = True
+        if snakeSize > 1:
+            for tail in tails: 
+                tail = pygame.draw.rect(screen, "green", (tail.left, tail.top, 30, 30), 0)
 
-        if moving == True:
-            if iterationCounter % difficulty == 0:
-                snake, lastDir = snakeMovement(keys, snake, lastDir)
+        if iterationCounter % difficulty == 0:
+            snake.move_ip(lastDir)
+            if snakeSize > 1:
+                tails[0].move_ip(lastDir)
+                print("tail x ", tails[0].x)
+                print("snake x", snake.x)
+            #for i in range(len(tails)):
+                #tails[i].move_ip(lastDir)
+                
+        snake, lastDir = changeDirection(keys, snake, lastDir, GRID_SIZE)
         
         edgeCollision(snake.left, snake.right, snake.top, snake.bottom)
-        score, snake = foodCollision(snake, food, score, screen)
-        drawGrid(screen)
+        score, snakeSize, tailPosition, tails = foodCollision(snake, screen, food, score, snakeSize, lastDir, GRID_SIZE, tailPosition, tails)
 
+        drawGrid(screen, SCREEN_WIDTH, SCREEN_HEIGHT, GRID_SIZE)
         # flip() the display to put your work on screen
         pygame.display.flip()
-
+        
         #TODO FIX SCORE VIEW
         scoreMsg = "Current score: " + str(score)
         pygame.display.set_caption(scoreMsg)
@@ -66,44 +78,64 @@ def edgeCollision(left, right, top, bottom):
         print("Collision with edge. Fail.")
         main()
 
-def foodCollision(snake, food, score, screen):
+def foodSpawn(food):
+    food.centerx = 15+(30*(random.randint(0,23)))
+    food.centery = 15+(30*(random.randint(0,23)))
+
+def foodCollision(snake, screen, food, score, snakeSize, lastDir, GRID_SIZE, tailPosition, tails):
     if snake.colliderect(food):
         foodSpawn(food)
-        snake = snakeGrowth(snake, screen, food)
-        score += 100
-    return score, snake 
+        score += 10
+        snakeSize += 1
+        tailPosition = snakeGrowth(snake, lastDir, snakeSize, tailPosition, GRID_SIZE)
+        tails = updateTails(tailPosition, screen, tails)
+    return score, snakeSize, tailPosition, tails
 
-def foodSpawn(food):
-    foodX = 15+(30*(random.randint(0,23)))
-    foodY = 15+(30*(random.randint(0,23)))
-    food.move_ip(foodX, foodY)
+def updateTails(tailPosition, screen, tails):
+    for position in tailPosition:
+        newTail = pygame.draw.rect(screen, "green", (position[0], position[1], 30, 30), 0)
+        tails.append(newTail)
+        return tails
 
-#TO BE CHANGED
-#Add new rect at end of snake, somehow make them move together
-def snakeGrowth(snake, screen, food):
-    tail = pygame.draw.rect(screen, "green", (food.centerx-60, food.centery-60, 30, 30), 0)
-    snake = snake.union(tail)
+def snakeGrowth(snake, lastDir, snakeSize, tailPosition, GRID_SIZE):
+    (tailX, tailY) = tailSpawn(lastDir, snake, GRID_SIZE, snakeSize)
+    tailPosition.append((tailX, tailY))
+    print(tailPosition)
+    return tailPosition
 
-def drawGrid(screen):
-    for i in range(0, 720, 30):
-        pygame.draw.line(screen, (100, 100, 100), (0, i), (720, i))
-        pygame.draw.line(screen, (100, 100, 100), (i, 0), (i, 720))
+def tailSpawn(lastDir, snake, GRID_SIZE, snakeSize):
+    dirX = lastDir[0]
+    dirY = lastDir[1]
+    if dirX == 0:
+        if dirY > 0:
+            (x, y) = (snake.x, snake.y - GRID_SIZE/2*snakeSize)
+        else:
+            (x, y) = (snake.x, snake.y + GRID_SIZE/2*snakeSize)
+    else:
+        if dirX > 0:
+            (x, y) = (snake.x - GRID_SIZE/2*snakeSize, snake.y)
+        else:
+            (x, y) = (snake.x + GRID_SIZE/2*snakeSize, snake.y)
+    return (x, y)
+
+def drawGrid(screen, SCREEN_WIDTH, SCREEN_HEIGHT, GRID_SIZE):
+    for i in range(0, SCREEN_WIDTH, GRID_SIZE):
+        pygame.draw.line(screen, (100, 100, 100), (0, i), (SCREEN_WIDTH, i))
+        pygame.draw.line(screen, (100, 100, 100), (i, 0), (i, SCREEN_HEIGHT))
     pygame.display.update()    
 
-def snakeMovement(keys, snake, lastDir):
-    #TODO FIX KEY PRIORITY
+def changeDirection(keys, snake, lastDir, GRID_SIZE):
     dx, dy = 0, 0
     if keys[pygame.K_a]:
-        dx -= 30
+        dx -= GRID_SIZE
     elif keys[pygame.K_d]:
-        dx += 30
+        dx += GRID_SIZE
     elif keys[pygame.K_w]:
-        dy -= 30
+        dy -= GRID_SIZE
     elif keys[pygame.K_s]: 
-        dy += 30
+        dy += GRID_SIZE
     else:
         (dx,dy) = lastDir
-    snake.move_ip(dx, dy)
     return snake, (dx, dy)
 
 

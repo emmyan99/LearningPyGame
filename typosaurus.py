1478d4de57d811136a7baf06d9d924a59e9631cf
emmyan99 <emmy.andreasson@gmail.com>
2024-10-05 19:10:53 +0200

    broken highlighting, cool font B-)

--- typosaurus.py
+++ typosaurus.py
@@ -3,24 +3,26 @@ import pygame
 import sys
 import random
 
-#TODO: more words and text wrapping
+#TODO!!!!!!!: fix highlighting of correctly typed charactesr, indexing 
+#TODO: text wrapping 
 #TODO: wpm count
-#TODO: colour each char once pressed instead of deleting off screen
+#TODO: leaderboard? menu screen? 
 
 
 # pygame setup
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 500
+INPUT_WIDTH = 500
+INPUT_HEIGHT = 250
 pygame.init()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT]) 
-#dt = 0
 
 #font for user typed text
-base_font = pygame.font.Font(None, 32) 
+base_font = pygame.font.Font("ka1.ttf", 32) 
 user_text = '' 
 
-input_rect = pygame.Rect(50, 100, 500, 250) 
+input_rect = pygame.Rect(50, 100, INPUT_WIDTH, INPUT_HEIGHT) 
 
 color_active = pygame.Color('gray90')
 color_passive = pygame.Color('gray95') 
@@ -34,17 +36,38 @@ indexCounter = 0
 indexes = []
 running = True
 pickwords = True
-while running:
+
 
     # pick 10 random words from allwords
-    while pickwords:
-        words = random.sample(allwords, k=2)
-        pickwords = False
-        words = ' '.join(words)
-        wordsToDisplay = words
-        print(words)
+while pickwords:
+    words = random.sample(allwords, k=10)
+    pickwords = False
+    words = ' '.join(words)
+    wordsToDisplay = words
+    print(words)
+
+#wordsToDisplay, INPUT_WIDTH
+def wrap_text(text, max_width, font):
+    lines = []
+    words = text.split(' ') #["apple", "banana", "cherry"]
+    current_line = ""
+
+    for word in words: #"apple"
+        test_line = current_line + word + ' ' #"apple "
+        if font.size(test_line)[0] <= max_width: #if line fits within box
+            current_line = test_line # add word 
+        else:
+            lines.append(current_line) # append line 
+            current_line = word + ' '
+
+    if current_line: # remaining words 
+        lines.append(current_line)
+
+    return lines       
 
 
+
+while running:
 
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -70,7 +93,6 @@ while running:
                         print("correct")
                         indexes.append(indexCounter)
                         indexCounter += 1
-                        print(indexes)
                     elif " " == words[0]:
                         if event.key == pygame.K_SPACE:
                             words = words[1:]
@@ -99,19 +121,24 @@ while running:
     # draw input rectangle 
     pygame.draw.rect(screen, color, input_rect) 
 
-    x_position = input_rect.x+5
-    y_position = input_rect.y+5
+    #x_position = input_rect.x+10
+    y_position = input_rect.y+10
 
-    for i, char in enumerate(wordsToDisplay):
-        if not i in indexes:
-            text_surface = base_font.render(char, True, ("Black"))
-        else:
-            text_surface = base_font.render(char, True, ("Red")) 
+    wrapped_lines = wrap_text(wordsToDisplay, INPUT_WIDTH-10, base_font)
+    for line in wrapped_lines:
+        x_position = input_rect.x + 10 # reset x position for each new line
+
+        for i, char in enumerate(line):
+            if i in indexes:
+                text_surface = base_font.render(char, True, ("Red"))
+            else:
+                text_surface = base_font.render(char, True, ("Black")) 
 
 
-        # render at position stated in arguments 
-        screen.blit(text_surface, (x_position, y_position)) 
-        x_position += text_surface.get_width()
+            # render at position stated in arguments 
+            screen.blit(text_surface, (x_position, y_position)) 
+            x_position += text_surface.get_width()  # Move x position to the right
+        y_position += base_font.get_height()  # Move down to the next line
 
       
     # set width of textfield so that text cannot get 
@@ -128,3 +155,4 @@ while running:
     dt = clock.tick(60)
 
 pygame.quit()
+
